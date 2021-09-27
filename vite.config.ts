bbe64c1277ea8201c34d57a65f96ab002d9f3dd0
j1ngzoue <workdrive0306@gmail.com>
2021-09-27 12:16:24 +0900

    lemon

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { defineConfig } from "vite";
+import { UserConfigExport } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import Pages from "vite-plugin-pages";
+import devcert from "devcert";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    Pages({
-      pagesDir: [{ dir: "src/pages", baseRoute: "" }],
-      extensions: ["tsx"],
-      react: true,
-    }),
-  ],
-  server: {
-    port: 8080,
-  },
-});
+export default async (): Promise<UserConfigExport> => {
+  const { key, cert } = await devcert.certificateFor("localhost");
+
+  return {
+    plugins: [
+      reactRefresh(),
+      Pages({
+        pagesDir: [{ dir: "src/pages", baseRoute: "" }],
+        extensions: ["tsx"],
+        react: true,
+      }),
+    ],
+    server: {
+      port: 8080,
+      https: {
+        key,
+        cert,
+      },
+    },
+  };
+};
